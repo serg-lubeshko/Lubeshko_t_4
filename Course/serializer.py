7ae8fa33f34bd25fbdbc552245acbc953552e0c6
Sergei_L <lubeshkosergei@gmail.com>
2021-12-17 21:07:27 +0300

    Management
    Develope for filling base

--- Course/serializer.py
+++ Course/serializer.py
@@ -39,10 +39,10 @@ class TeachCourSerializer(serializers.ModelSerializer):
     #     return prof
 
 
-# class TeacherAddSerializer(serializers.ModelSerializer):
-#     author = UserSerializer(read_only=True)
-#     teacher = UserSerializer()
-#
-#     class Meta:
-#         model = Course
-#         fields = ['id', 'author', 'name', 'description', 'published_at', 'update_at', 'teacher']
+class TeacherAddSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+    teacher = UserSerializer()
+
+    class Meta:
+        model = Course
+        fields = ['id', 'author', 'name', 'description', 'published_at', 'update_at', 'teacher']

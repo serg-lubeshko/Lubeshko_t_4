e911c5fcca82ba228d2f04012ada87dc59717ce8
Sergei_L <lubeshkosergei@gmail.com>
2021-12-22 23:08:23 +0300

    add validate to HomeworkSerializer

--- projects/Homework/serializers.py
+++ projects/Homework/serializers.py
@@ -1,33 +1,29 @@
 from rest_framework import serializers
-from rest_framework.validators import UniqueTogetherValidator
 
 from projects.Homework.models import Homework
 from projects.Lecture.models import Lecture
 from projects.Lecture.serializers import LectureSerializer
 
 
-
-
-
 class HomeworkSerializer(serializers.ModelSerializer):
-    a = 1
     professor = serializers.PrimaryKeyRelatedField(read_only=True)
     lecture_for_homework = serializers.ChoiceField(
-        choices=[i.id for i in Lecture.objects.filter(professor_id=a)],
+        choices=[i.id for i in Lecture.objects.all()],
         source='lecture_for_homework_id')
 
     class Meta:
         model = Homework
         fields = ['id', 'title', 'homework_task', 'published_at', 'professor', 'lecture_for_homework']
 
-
-    def validate_homework_task(self, value):
-        a=HomeworkSerializer().user = self.context['request'].user.pk
+    def validate(self, data):
         user_id = self.context['request'].user.pk
         lecture_id = self.context['request'].data['lecture_for_homework']
+        data_dict = dict(data)
         if Homework.objects.filter(professor_id=user_id, lecture_for_homework=lecture_id).count() > 0:
             raise serializers.ValidationError("Вы добавили уже домашнюю работу")
-        return value
+        if Lecture.objects.filter(id=data_dict['lecture_for_homework_id']).first().professor.pk != user_id:
+            raise serializers.ValidationError("У вас нет прав")
+        return data
 
 
 class LectureFofHomework(LectureSerializer):
